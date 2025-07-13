6ee0954ba1ac9e0bae2bdc7bc820ff166d7c1640
Shashwat Singh <shashawat15@gmail.com>
2025-07-13 21:32:40 +0530

    Add files via upload

---
+++ cross.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << "Invalid input print an odd number plzz." << endl;
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == j || i + j == n + 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
